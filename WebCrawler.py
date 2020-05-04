6c196fb44359fec4375dc4fb794e05acdc62d757
TCPizza <nbletel@gmail.com>
2020-05-05 01:54:54 +0300

    Working V1, update and insert fully working

--- WebCrawler.py
+++ WebCrawler.py
@@ -2,28 +2,81 @@ import pymongo
 from requests import get
 import bs4 as bs
 
-def DataBaseInit():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb+srv://TCPizza:1478963250n@datacrawled-i1kaj.mongodb.net/test?retryWrites=true&w=majority")
 
-    mydb = myclient["mydatabase"]
-    myCol = mydb["Brand"]
-def PyCrawler():
-    DataBaseInit()
-    
+FirmwareDB = client["DATA"]
+metadata = FirmwareDB["metadata"]
+
+
+def main(*args):
+    for arg in args:
+        pass
+    PageCrawl()
+
+
 
 def PageCrawl():
-    current_page = list()
-    for index in range(7):
-        current_page.append(get("http://www.rockchipfirmware.com/firmware-downloads-5?page="+str(index)).text)
-    html_soup = bs(current_page, 'html.parser')
-    html_soup.find_all('div', class_ = 'views-field views-field-field-brand')
+    pages = list()
+    id = 0 #unique index to check if there is a duplicate
+    numberOfPages = findAmountOfPagesToCrawl()
+    #get all pages into a list for further parsing
+    for index in range(int(numberOfPages)+1):
+        pages.append(get("http://www.rockchipfirmware.com/firmware-downloads?page="+str(index)).text)
+    for page in pages:
+        html_soup = bs.BeautifulSoup(page, 'html.parser')
+        data = html_soup.find_all('tr', class_ = ['odd','even','odd views-row-first','even views-row-last'])
+        for item in data:
+            item = createObject(item, id)
+            if item == metadata.find_one({"_id":id}):
+                print("DUPLICATE ALERT")
+            else:
+                try:
+                    metadata.update_one(metadata.find_one({"_id":id}), item)
+                except:
+                    metadata.insert_one(item)
+            id += 1
+            #print(id)
 
 
+'''
+create object to add to mongoDB with the folowing fields:
+id, model, name, Node(refrence for downloading file),andriod version, Author
+returns as dict to add to mongoDB
+'''        
+def createObject(item, id):
+    FRef = ""
+    node = item.find_all('a',href=True)[0]['href']
+    if '\\' in node:
+        node = node.replace('\\','/')
+    #print(node)
+    File = (bs.BeautifulSoup(get("http://www.rockchipfirmware.com/"+node).text, 'html.parser')).find_all('a',href=True)
+    #print(File)
+    for x in File:
+        if "rockchip" in x['href']:
+            FRef = x['href']
+            break
 
+    return {"_id":id,
+            "Brand":item.find('td', class_ = "views-field views-field-field-brand").get_text(strip=True),
+            "Model":item.find('td', class_ = "views-field views-field-field-model").get_text(strip=True),
+            "Name":item.find('td', class_ = "views-field views-field-title").get_text(strip=True),
+            "Node":node,
+            "FileRef":FRef,
+            "AndriodVersion":item.find('td', class_ = "views-field views-field-field-android-version2").get_text(strip=True)
+            ,"Author":item.find('td', class_ = "views-field views-field-field-firmware-author").get_text(strip=True)}
+
+###FULLY WORKING
+#for a dynamic Crawl 
+#the number of pages could change so i take the current amount
 def findAmountOfPagesToCrawl():
-    htmlS = bs( get("http://www.rockchipfirmware.com/firmware-downloads-5?page=0").text, 'html.parser')
-    res = str(htmlS.find('div', class_ = 'pager-current').get_text(strip=True))
-    return res[res.rindex(' '):len(res)]
+    htmlS = bs.BeautifulSoup(get("http://www.rockchipfirmware.com/firmware-downloads-5?page=0").text, 'html.parser')
+    #get text in format "1 to <lastPage>"
+    res = htmlS.find('li', class_ = 'pager-current').get_text(strip=True)
+
+    #Return index of last page
+    return res[res.rindex(' ')+1:len(res)]
+
+
 if __name__ == "__main__":    
-    crawler = PyCrawler()    
-    crawler.start()
+    main()    
+    
